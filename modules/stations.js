afcbd15d463664caaf77912465f6f4cc1623a577
Jan <github@sintho.com>
2020-03-04 20:28:03 +0100

    Added station names for abbreviations added 20px to box with for long station names

---
+++ modules/stations.js
@@ -0,0 +1,185 @@
+const Stations = {
+    //S1
+    MFR: 'Freising',
+    MPU: 'Pulling',
+    //--> s. S8
+    //-->
+    MNF: 'Neufahrn',
+    MEC: 'Eching',
+    MLH: 'Lohhof',
+    MUSM: 'Unterschleißheim',
+    MOSM: 'Oberschleißheim',
+    MFE: 'Feldmoching',
+    MFAS: 'Fasanerie',
+    MMCH: 'Moosach',
+    //Stammstrecke
+
+    //S2
+    MER: 'Erding',
+    MANE: 'Altenerding',
+    MAFE: 'Aufhausen',
+    MKL: 'St. Koloman',
+    MONH: 'Ottenhofen',
+    MSB: 'Markt Schwaben',
+    MPO: 'Poing',
+    MGRB: 'Grub',
+    MHEM: 'Heimstetten',
+    MFK: 'Feldkirchen',
+    'MRI P': 'Riem',
+    //Stammstrecke
+    MOZ: 'Obermenzing',
+    MAUG: 'Untermenzing',
+    MMAL: 'Allach',
+    MKFS: 'Karlsfeld',
+    MDA: 'Dachau',
+    //-->
+    MHSN: 'Hebertshausen',
+    MRMS: 'Röhrmoos',
+    MESH: 'Vierkirchen-Esterhofen',
+    MPE: 'Petershausen',
+    //-->
+    MDAS: 'Dachau Stadt',
+    MBCN: 'Bachern',
+    MSHH: 'Schwabhausen',
+    MNRO: 'Niederroth',
+    MIDR: 'Markt Indersdorf',
+    MANB: 'Arnbach',
+    MEWG: 'Erdweg',
+    MKHN: 'Kleinberghofen',
+    MAMT: 'Altomünster',
+
+    //S3
+    MMAM: 'Mammendorf',
+    MMAG: 'Malching',
+    MMA: 'Maisach',
+    MGLD: 'Gernlinden',
+    MESG: 'Esting',
+    MOL: 'Olching',
+    MGRZ: 'Gröbenzell',
+    MLO: 'Lochhausen',
+    MLW: 'Langwied',
+    //Stammstrecke
+    MMAR: 'St.-Martin-Straße',
+    MGI: 'Giesing',
+    MFG: 'Fasanengarten',
+    MFP: 'Fasanenpark',
+    MUH: 'Unterhaching',
+    MTU: 'Taufkirchen',
+    MFU: 'Furth',
+    MDS: 'Deisenhofen',
+    MSR: 'Sauerlach',
+    MOTF: 'Otterfing',
+    MHO: 'Holzkirchen',
+
+    //S4
+    MGE: 'Geltendorf',
+    MTFD: 'Türkenfeld',
+    MGF: 'Grafrath',
+    MSNG: 'Schöngeising',
+    MBAU: 'Buchenau',
+    MFB: 'Fürstenfeldbruck',
+    MEIC: 'Eichenau',
+    MPM: 'Puchheim',
+    MMAU: 'Aubing',
+    MLEF: 'Leienfelsstraße',
+    //Stammstrecke
+    //--> s. S6
+
+    //S6
+    MTZ: 'Tutzing',
+    MFA: 'Feldafing',
+    MPH: 'Possenhofen',
+    MST: 'Starnberg',
+    MSNO: 'Starnberg Nord',
+    MGT: 'Gauting',
+    MSD: 'Stockdorf',
+    MPL: 'Planegg',
+    MGFL: 'Gräfelfing',
+    MLCH: 'Lochham',
+    //Stammstrecke
+    MTR: 'Trudering', //auch S4
+    MGDF: 'Gronsdorf',
+    MHR: 'Haar',
+    MVS: 'Vaterstetten',
+    MBDH: 'Baldham',
+    MZO: 'Zorneding',
+    MEGL: 'Eglharting',
+    'MKO H': 'Kirchseeon',
+    MGB: 'Grafing Bahnhof',
+    MGRS: 'Grafing Stadt',
+    MEG: 'Ebersberg',
+
+    //S7
+    MWO: 'Wolfratshausen',
+    MIC: 'Icking',
+    MEBS: 'Ebenhausen-Schäftlarn',
+    MHSL: 'Hohenschäftlarn',
+    MBAB: 'Baierbrunn',
+    MBHA: 'Buchenhain',
+    MHRK: 'Höllriegelskreuth',
+    MPUL: 'Pullach',
+    MGOI: 'Großhesselohe Isartalbahnhof',
+    MSN: 'Solln',
+    MSW: 'Siemenswerke',
+    MMT: 'Mittersendling',
+    MHAR: 'Harras',
+    MHPW: 'Heimeranplatz',
+    //Stammstrecke
+    //s. S3
+    MPER: 'Perlach',
+    MNPS: 'Neuperlach Süd',
+    MNB: 'Neubiberg',
+    MOBR: 'Ottobrunn',
+    MHOB: 'Hohenbrunn',
+    MWAE: 'Wächterhof',
+    MHSB: 'Höhenkrichen-Siegertsbrunn',
+    MDHR: 'Dürrnhaar',
+    MAY: 'Aying',
+    MPEI: 'Peiß',
+    MGHD: 'Großhelfendorf',
+    MKZ: 'Kreuzstraße',
+
+    //S8
+    MHI: 'Herrsching',
+    MSH: 'Seefeld-Hechendorf',
+    MSA: 'Steinebach',
+    MWS: 'Weßling',
+    MNGH: 'Neugilching',
+    MGCH: 'Gilching-Argelsried',
+    MGIB: 'Geisenbrunn',
+    MUG: 'Germering',
+    MHHS: 'Harthaus',
+    MFHH: 'Freiham',
+    MNA: 'Neuaubing',
+    //S8 Stammstrecke
+    MDFG: 'Daglfing',
+    MEGS: 'Englschalking',
+    MJK: 'Joahnneskirchen',
+    MUFG: 'Unterföhring',
+    MIS: 'Ismaning',
+    MHMO: 'Hallbergmoos',
+    MFHB: 'Flughafen Besucherpark', //auch S1
+    MFHM: 'Flughafen/Airport',
+
+
+    //Stammstrecke
+    MWKR: 'Westkreuz', //S6, S8
+    MP: 'Pasing',
+    ML: 'Laim',
+    MMHG: 'Hirschgarten',
+    MMDN: 'Donnersbergerbrücke',
+    MHAB: 'Hackerbrücke',
+    MHT: 'Hauptbahnhof',
+    MKA: 'Karlsplatz/Stachus',
+    MMP: 'Marienplatz',
+    MIT: 'Isartor',
+    MRP: 'Rosenheimer Platz',
+    MOP: 'Ostbahnhof',
+    MLEU: 'Leuchtenbergring', //S1, S2, S4, S6, S8
+    MBAL: 'Berg am Laim', //S2, S4, S6
+
+    //S20
+    //s. S7
+};
+
+export default Stations;
