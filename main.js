724f00622a54646fff90b880f3bb2feba2d4102f
Jan <github@sintho.com>
2020-03-09 19:40:04 +0100

    fixed Hbf Gleis 27-36, as it contains two spaces instead of just one added empty string for cases where no prevStation is set

--- main.js
+++ main.js
@@ -79,7 +79,7 @@ class SBahnGui {
                 if (Object.keys(train.vehicles).length > 0) actions.push('an Wagen ' + Object.keys(train.vehicles).join('+') + ' angekuppelt');
 
                 if (actions.length > 0) {
-                    this.log(`${(new Date()).toLocaleTimeString()}: ${Stations[train.prevStation] || train.prevStation}: Wagen ${trainInfo.vehicle_number} wurde ${actions.join(' und ')}`);
+                    this.log(`${(new Date()).toLocaleTimeString()}: ${Stations[train.prevStation] || train.prevStation || ''}: Wagen ${trainInfo.vehicle_number} wurde ${actions.join(' und ')}`);
                 }
             }
             this.waggons[trainInfo.vehicle_number] = train;
