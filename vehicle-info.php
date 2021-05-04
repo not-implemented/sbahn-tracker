ba765a2ee7c062ea026899d21bf27a6a8ecf7a40
Mark Plomer <mark.plomer@boerse-go.de>
2021-05-04 13:47:33 +0200

    Nicht-modernisierte Bahnen mit 'Hagen|Nürnberg|Krefeld' im Kommentar auch als outdated kennzeichnen

--- vehicle-info.php
+++ vehicle-info.php
@@ -5,7 +5,7 @@ $config = require 'config.php';
 $db = new PDO($config->dsn, $config->user, $config->password);
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
 
-$vehicleInfos = $db->query('SELECT model, `number`, isModern, IF(`lastSeenAt` < \'2021-02-22\' OR `lastSeenAt` IS NULL, 1, 0) AS isOutdated FROM vehicle_info')->fetchAll();
+$vehicleInfos = $db->query('SELECT model, `number`, isModern, IF(`lastSeenAt` < \'2021-02-22\' OR `lastSeenAt` IS NULL OR isModern = 0 AND comment REGEXP \'Hagen|Nürnberg|Krefeld\', 1, 0) AS isOutdated FROM vehicle_info')->fetchAll();
 
 foreach ($vehicleInfos as $vehicleInfo) {
     if ($vehicleInfo->isModern !== null) $vehicleInfo->isModern = (bool) $vehicleInfo->isModern;
